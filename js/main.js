9cece1061c247d434cbe058c3236831e0b8df4af
Piotr Kolawa <p.kolawa@gmail.com>
2017-03-09 00:07:26 +0100

    Dorabianie RWD

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function mainRingResize(){
 
 	if(windowWidth > 600){
 		if(windowWidth > (windowHeight - headerHeight - footerHeight)){
-			var mainRingHeight = windowHeight - headerHeight - footerHeight - 100;
+			var mainRingHeight = windowHeight - headerHeight - footerHeight - 10;
 		}else{
 			var mainRingHeight = windowWidth - 200;
 		}
